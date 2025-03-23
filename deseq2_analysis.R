734a70f55c24db9251b07f7cc8ab2e1226dfccf3
Kingabioinformatic <zygmuntowicz.kin@gmail.com>
2025-03-23 17:43:11 -0400

    Create deseq2_analysis.R
    The main goal of this script is to perform differential gene expression analysis using DESeq2. It processes RNA-Seq count data to identify genes that are differentially expressed between experimental conditions. This script can be run either in RStudio or directly from the command line.

---
+++ deseq2_analysis.R
@@ -0,0 +1,18 @@
+# Load necessary libraries
+library(DESeq2)
+
+# Load count data and sample information
+counts <- read.table("counts.txt", header=TRUE, row.names=1)
+coldata <- data.frame(condition=c("control", "treatment"))  # Replace with actual sample conditions
+
+# Create DESeqDataSet object
+dds <- DESeqDataSetFromMatrix(countData = counts, colData = coldata, design = ~ condition)
+
+# Perform DESeq2 analysis
+dds <- DESeq(dds)
+
+# Obtain results
+res <- results(dds)
+
+# Write results to a CSV file
+write.csv(as.data.frame(res), file="zygmukin_deseq2_results.csv")
